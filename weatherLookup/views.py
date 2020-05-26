d3831867f4476e34f342b2737f8e6cb92f0167da
amroakmal <amrakmal97@gmail.com>
2020-05-26 05:04:46 +0200

    Handle POST requests.

--- weatherLookup/views.py
+++ weatherLookup/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
+from django import forms
 
 # Create your views here.
 def home(request):
     import json, requests
-    zip_code = "20002"
-    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=" + zip_code + "&distance=25&API_KEY=A3ADA6EC-7329-49CF-B291-19D16733441A")
-    
-    try:
-        api = json.loads(api_request.content)
+    if request.method == "POST":
+        zip_code = request.POST['zipcode']
+        api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=" + zip_code + "&distance=25&API_KEY=A3ADA6EC-7329-49CF-B291-19D16733441A")
+        try:
+            api = json.loads(api_request.content)
+        except Exception:
+            api = 'Error..'
+        categoryColor = ""
+        category = api[0]['Category']['Name']
+        if category == "Good":
+            categoryColor = "good"
+        elif category == "Moderate":
+            categoryColor = "moderate"
+        elif category == "Unhealty for Sensitive Groups":
+            categoryColor = "usg"
+        elif category == "Unhealthy":
+            categoryColor = "unhealthy"
+        elif category == "Very Unhealthy":
+            categoryColor = "veryUnhealthy"
+        else:
+            categoryColor = "hazardous"
+        return render(request, 'home.html', {'api': api, 'categoryColor': categoryColor})
+    else:
+        api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=A3ADA6EC-7329-49CF-B291-19D16733441A")
+        try:
+            api = json.loads(api_request.content)
+        except Exception:
+            api = 'Error..'
         categoryColor = ""
         category = api[0]['Category']['Name']
         if(category == "Good"):
@@ -22,9 +46,7 @@ def home(request):
             categoryColor = "veryUnhealthy"
         else:
             categoryColor = "hazardous"
-    except Exception as e:
-        api = 'Error..'
-    return render(request, 'home.html', {'api': api, 'categoryColor': categoryColor})
-
+        return render(request, 'home.html', {'api': api, 'categoryColor': categoryColor})
+   
 def about(request):
     return render(request, 'about.html')
