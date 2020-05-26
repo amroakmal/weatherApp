4698b92da2c2ec2d71ace5eabac62977ff1cccad
amroakmal <amrakmal97@gmail.com>
2020-05-26 03:02:33 +0200

    Make API request

--- weatherLookup/views.py
+++ weatherLookup/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    import json, requests
+    zip_code = "20002"
+    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=" + zip_code + "&distance=25&API_KEY=A3ADA6EC-7329-49CF-B291-19D16733441A")
+    
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = 'Error..'
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html')
