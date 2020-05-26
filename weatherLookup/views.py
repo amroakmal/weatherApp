dc6448c215a82aad25a00602eaf049553f253218
amroakmal <amrakmal97@gmail.com>
2020-05-26 05:12:31 +0200

    Handle invalid requests.

--- weatherLookup/views.py
+++ weatherLookup/views.py
@@ -9,23 +9,24 @@ def home(request):
         api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=" + zip_code + "&distance=25&API_KEY=A3ADA6EC-7329-49CF-B291-19D16733441A")
         try:
             api = json.loads(api_request.content)
+            categoryColor = ""
+            category = api[0]['Category']['Name']
+            if category == "Good":
+                categoryColor = "good"
+            elif category == "Moderate":
+                categoryColor = "moderate"
+            elif category == "Unhealty for Sensitive Groups":
+                categoryColor = "usg"
+            elif category == "Unhealthy":
+                categoryColor = "unhealthy"
+            elif category == "Very Unhealthy":
+                categoryColor = "veryUnhealthy"
+            else:
+                categoryColor = "hazardous"
+            return render(request, 'home.html', {'api': api, 'categoryColor': categoryColor})
         except Exception:
-            api = 'Error..'
-        categoryColor = ""
-        category = api[0]['Category']['Name']
-        if category == "Good":
-            categoryColor = "good"
-        elif category == "Moderate":
-            categoryColor = "moderate"
-        elif category == "Unhealty for Sensitive Groups":
-            categoryColor = "usg"
-        elif category == "Unhealthy":
-            categoryColor = "unhealthy"
-        elif category == "Very Unhealthy":
-            categoryColor = "veryUnhealthy"
-        else:
-            categoryColor = "hazardous"
-        return render(request, 'home.html', {'api': api, 'categoryColor': categoryColor})
+            api = "Error"
+            return render(request, 'home.html', {'api': api})
     else:
         api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=A3ADA6EC-7329-49CF-B291-19D16733441A")
         try:
